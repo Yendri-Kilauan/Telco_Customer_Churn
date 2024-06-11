f032e04cd09bf3474e4fff6535dcb3dd7b34ed88
Yendri Kilauan Purnama <134827728+Yendri-Kilauan@users.noreply.github.com>
2024-06-11 09:31:40 +0700

    Add files via upload

---
+++ source code/streamlit/app1.py
@@ -0,0 +1,84 @@
+# LogisticRegression
+# file streamlit = app1.py 
+# file model = logistic.joblib
+
+import joblib
+import streamlit as st
+
+model_file = 'logistic.joblib'
+
+pickle_in = open(model_file, 'rb')   # Unpickling
+classifier = joblib.load(pickle_in)
+
+st.set_page_config(page_title='ChurnPredictor', page_icon="🖖")    # The set_page_config method lets us define the webpage title name and icon
+
+@st.cache_data()
+# Define the function which will make the prediction using data inputs from users
+def prediction(seniorcitizen, partner, dependents, tenure, phoneservice, multiplelines,
+               internetservice, onlinesecurity, onlinebackup, deviceprotection, techsupport,
+               streamingtv, streamingmovies, paperlessbilling, monthlycharges, M,
+               one_year, two_year, credit_card, electronic_check, mailed_check):
+
+    # Make predictions
+    prediction = classifier.predict(
+        [[seniorcitizen, partner, dependents, tenure, phoneservice, multiplelines,
+          internetservice, onlinesecurity, onlinebackup, deviceprotection, techsupport,
+          streamingtv, streamingmovies, paperlessbilling, monthlycharges, M,
+          one_year, two_year, credit_card, electronic_check, mailed_check]])
+
+    if prediction == 0:
+        pred = 'Everything Looks good. The Customer is Loyal!'
+    else:
+        pred = 'Oh No! The customer is gonna CHURN! **Better do something about it**'
+    return pred
+
+# This is the main function in which we define our webpage
+def main():
+
+    st.title("The Churn Predictor Model")   # Title of the model displayed in the webpage
+
+    # Give a little bit information of the Model
+    st.info('The Model takes in the below predictor variables for a telecom company and predicts if a customer is going to churn or not!', icon="ℹ️")
+
+    # Create input fields
+    seniorcitizen = st.number_input("Are you a senior citizen ? ('1' for Yes and '0' for NO)", min_value=0, max_value=1, value=0, step=1)
+    partner = st.number_input("Do you have a partner ? ('1' for Yes and '0' for NO) ", min_value=0, max_value=1, value=0, step=1)
+    dependents = st.number_input("Do you have any dependents ? ('1' for Yes and '0' for NO) ", min_value=0, max_value=1, value=0, step=1)
+    tenure = st.number_input("Enter the months the customer (tenure) has stayed with the company(0-72)", min_value=0, max_value=72, value=10, step=3)
+    phoneservice = st.number_input("Did the customer have a phone service ? ('1' for Yes and '0' for NO)", min_value=0, max_value=1, value=0, step=1)
+    multiplelines = st.number_input("Did the customer have a multiplelines ? ('1' for Yes and '0' for NO)", min_value=0, max_value=1, value=0, step=1)
+    internetservice = st.number_input("Did the customer have a internetservices ? ('N' : 0, 'DSL' : 1, 'FO (Fiber Optic)' : 2)", min_value=0, max_value=2, value=0, step=1)
+    onlinesecurity = st.number_input("Did the customer have a onlinesecuritys ? ('1' for Yes and '0' for NO)", min_value=0, max_value=1, value=0, step=1)
+    onlinebackup = st.number_input("Did the customer have a onlinebackup ? ('1' for Yes and '0' for NO)", min_value=0, max_value=1, value=0, step=1)
+    deviceprotection = st.number_input("Did the customer have a deviceprotection ? ('1' for Yes and '0' for NO)", min_value=0, max_value=1, value=0, step=1)
+    techsupport = st.number_input("Did the customer have a techsupport ? ('1' for Yes and '0' for NO)", min_value=0, max_value=1, value=0, step=1)
+    streamingtv = st.number_input("Did the customer have a streamingtv ? ('1' for Yes and '0' for NO)", min_value=0, max_value=1, value=0, step=1)
+    streamingmovies = st.number_input("Did the customer have a streamingmovies ? ('1' for Yes and '0' for NO)", min_value=0, max_value=1, value=0, step=1)
+    paperlessbilling = st.number_input("Did the customer use paperless billing? ? ('1' for Yes and '0' for NO)", min_value=0, max_value=1, value=0, step=1)
+    monthlycharges = st.number_input("Enter the monthlycharges the customer (0-118.75)", min_value=0.0, max_value=118.75, value=10.0, step=3.0)
+    M = st.number_input("Are you a Male or Female ? ('1' for Male and '0' for Female)", min_value=0, max_value=1, value=0, step=1)
+    one_year = st.number_input("Does the customer have One year contract ? ('1' for Yes and '0' for NO)", min_value=0, max_value=1, value=0, step=1)
+    two_year = st.number_input("Does the customer have Two year contract ? ('1' for Yes and '0' for NO)", min_value=0, max_value=1, value=0, step=1)
+    credit_card = st.number_input("Did the customer use Credit_card? ? ('1' for Yes and '0' for NO)", min_value=0, max_value=1, value=0, step=1)
+    electronic_check = st.number_input("Did the customer use Electronic check ? ('1' for Yes and '0' for NO)", min_value=0, max_value=1, value=0, step=1)
+    mailed_check = st.number_input("Did the customer use Mailed check ? ('1' for Yes and '0' for NO)", min_value=0, max_value=1, value=0, step=1)
+
+
+    result = ""
+
+    # When 'Predict' is clicked, make the prediction and store it
+    if st.button("Predict"):
+        result = prediction(seniorcitizen, partner, dependents, tenure, phoneservice, multiplelines,
+                            internetservice, onlinesecurity, onlinebackup, deviceprotection, techsupport,
+                            streamingtv, streamingmovies, paperlessbilling, monthlycharges, M,
+                            one_year, two_year, credit_card, electronic_check, mailed_check)
+        st.success(result)
+        # If the predictions are true, celebrate that the model is properly working, else spit out a churn warning
+        if result == 'Everything Looks good. The Customer is Loyal!':
+            st.balloons()
+        else:
+            st.image('https://www.smartkarrot.com/wp-content/uploads/2020/09/Customer-churn-reduction.png',
+                     caption="Customer CHURN ALERT", width=150)
+
+if __name__ == '__main__':
+    main()
